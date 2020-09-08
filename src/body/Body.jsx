b7d8b89a394aa452eae382233e78af288949e02d
kbui314 <kbui3145@gmail.com>
2020-09-08 15:19:27 -0700

    First Commit

---
+++ src/body/Body.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Graph from '../graph/Graph';
+
+class Body extends Component{
+	render(){
+		return(
+			<div>
+				<Graph/>
+			</div>
+		)
+	};
+}
+export default Body;
+
---
+++ src/graph/Graph.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Graph extends Component{
+	render(){
+		return(
+			<div>
+				<h1>This is the graph</h1>
+			</div>
+		)
+	}
+}
+export default Graph;
